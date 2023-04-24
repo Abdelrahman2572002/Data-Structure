1bb1b508b1a8f26df4bca9ceff916c61ac23c253
Abdelrahman Ramadan <79172307+Abdelrahman2572002@users.noreply.github.com>
2023-04-24 18:27:32 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,43 +1,212 @@
-// =============================================================
-// Student: Ahmed Salman Fahmin
-// StudentID: 20200035
-// =============================================================
-// Student: Abdelrahamn Ramdan AboElela
-// StudentID: 20200284
-// =============================================================
-// Student: Mohamed Refaat Mohamed
-// StudentID: 20200443
-// =============================================================
-// Student: Adel Tarek Mouneer
-// StudentID: 20200800
-// =============================================================
-
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
-void RecPermute(string soFar, string rest)
+
+struct node
 {
-    set<string> names;
-if (rest == "") // No more characters
-cout << soFar << endl; // Print the word
-else // Still more chars
-// For each remaining char
-for (int i = 0; i < rest.length(); i++) {
-string next = soFar + rest[i]; // Glue next char
-string remaining = rest.substr(0, i)+ rest.substr(i+1);
-int length= names.size();
-names.insert(next);
-if(names.size()>length)
-RecPermute(next, remaining);
-}
-}
-// "wrapper" function
-void ListPermutations(string s) {
-RecPermute("", s);
-}
-int main()
+    string data;
+    node*next;
+};
+class Queue
 {
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    ListPermutations("makka");
-    return 0;
+public:
+    node *first,*last;
+    int count;
+    Queue()
+    {
+        count=0;
+        first=nullptr;
+        last=nullptr;
+    }
+    void push(string n)
+    {
+        count++;
+        if(count==1)
+        {
+
+            node *tmp;
+            tmp=new node;
+            tmp->data=n;
+            first=tmp;
+            last=tmp;
+        }
+        else
+        {
+            node *tmp=new node;
+            tmp->data=n;
+            last->next=tmp;
+            last=last->next;
+
+        }
+    }
+    string front()
+    {
+        if(count>0)
+            return first->data;
+        else
+        {
+            try
+            {
+                throw(" ");
+            }
+            catch(...)
+            {
+                cout<<"No Data To Show\n";
+                exit(505);
+
+
+            }
+        }
+    }
+    string back()
+    {
+        if(count>0)
+            return last->data;
+        else
+        {
+            try
+            {
+                throw(" ");
+            }
+            catch(...)
+            {
+                cout<<"No Data To Show\n";
+                exit(505);
+
+            }
+        }
+    }
+    void pop()
+    {
+        if(count>0)
+        {
+            node *tmp=first;
+            first=first->next;
+            delete tmp;
+            count--;
+        }
+        else
+        {
+            try
+            {
+                throw(" ");
+            }
+            catch(...)
+            {
+                cout<<"No Data To Erase\n";
+                exit(505);
+
+            }
+        }
+    }
+    int size()
+    {
+        return count;
+    }
+};
+class Stack
+{
+private:
+    Queue qk;
+public:
+    string top()
+    {
+        if(qk.size()>0)
+            return qk.front();
+        else
+        {
+            try
+            {
+                throw("NO Data \n");
+            }
+            catch(...)
+            {
+                cout<<"NO Data \nThis Value is random :  ";
+            }
+        }
+
+    }
+    void push(string n)
+    {
+        int s=qk.size();
+        qk.push(n);
+        for(int i=0; i<s; i++)
+        {
+            qk.push(qk.front());
+            qk.pop();
+        }
+    }
+
+    void pop()
+    {
+        if(qk.size()>0)
+            qk.pop();
+        else
+        {
+            try
+            {
+                throw("NO Data \n");
+            }
+            catch(...)
+            {
+                cout<<"NO Data To Erase \n";
+                exit(505);
+            }
+        }
+
+
+    }
+    int size()
+    {
+        return qk.size();
+    }
+    bool empty(){
+        return !size();
+    }
+};
+string simplify(string input){
+    Stack str;
+    string result;
+for(int i=0;i<input.size();i++){
+    if(input[i]=='/'){continue;}
+        string tmp;
+    while(i<input.size()&&input[i]!='/'){
+        tmp +=input[i];
+        ++i;
+        }
+    if(tmp=="."){continue;}
+    else if(tmp==".."){
+        if(!str.empty()){
+            str.pop();
+            }
+            }
+            else{
+                str.push(tmp);
+                }
+        }
+
+        while(!str.empty())
+        {
+            result = "/" + str.top() + result;
+            str.pop();
+        }
+
+        if(result.size() == 0){
+            return "/";
+            }
+
+        return result;
+}
+
+int main(){
+    //1
+ cout<<simplify("/home/")<<endl;
+    //2
+ cout<<simplify("/../")<<endl;
+    //3
+cout<<simplify("/home//foo/")<<endl;
+    //4
+cout<<simplify("/home/../abc/./foo/")<<endl;
+    //5
+cout<<simplify("/ezz/./ahmed/..////mostafa")<<endl;
 }
