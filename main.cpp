cdaccb87b3963ceffa932c64c0e75cd5cb857994
Abdelrahman Ramadan <79172307+Abdelrahman2572002@users.noreply.github.com>
2023-04-24 18:30:03 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,212 +1,102 @@
-#include <iostream>
 #include<bits/stdc++.h>
+
 using namespace std;
 
-struct node
+int search(string a, int rootVal)
 {
-    string data;
-    node*next;
-};
-class Queue
-{
-public:
-    node *first,*last;
-    int count;
-    Queue()
-    {
-        count=0;
-        first=nullptr;
-        last=nullptr;
-    }
-    void push(string n)
-    {
-        count++;
-        if(count==1)
-        {
-
-            node *tmp;
-            tmp=new node;
-            tmp->data=n;
-            first=tmp;
-            last=tmp;
-        }
-        else
-        {
-            node *tmp=new node;
-            tmp->data=n;
-            last->next=tmp;
-            last=last->next;
-
-        }
-    }
-    string front()
-    {
-        if(count>0)
-            return first->data;
-        else
-        {
-            try
-            {
-                throw(" ");
-            }
-            catch(...)
-            {
-                cout<<"No Data To Show\n";
-                exit(505);
-
-
-            }
-        }
-    }
-    string back()
-    {
-        if(count>0)
-            return last->data;
-        else
-        {
-            try
-            {
-                throw(" ");
-            }
-            catch(...)
-            {
-                cout<<"No Data To Show\n";
-                exit(505);
-
-            }
-        }
-    }
-    void pop()
-    {
-        if(count>0)
-        {
-            node *tmp=first;
-            first=first->next;
-            delete tmp;
-            count--;
-        }
-        else
-        {
-            try
-            {
-                throw(" ");
-            }
-            catch(...)
-            {
-                cout<<"No Data To Erase\n";
-                exit(505);
-
-            }
-        }
-    }
-    int size()
-    {
-        return count;
-    }
-};
-class Stack
-{
-private:
-    Queue qk;
-public:
-    string top()
-    {
-        if(qk.size()>0)
-            return qk.front();
-        else
-        {
-            try
-            {
-                throw("NO Data \n");
-            }
-            catch(...)
-            {
-                cout<<"NO Data \nThis Value is random :  ";
-            }
-        }
-
-    }
-    void push(string n)
-    {
-        int s=qk.size();
-        qk.push(n);
-        for(int i=0; i<s; i++)
-        {
-            qk.push(qk.front());
-            qk.pop();
-        }
-    }
-
-    void pop()
-    {
-        if(qk.size()>0)
-            qk.pop();
-        else
-        {
-            try
-            {
-                throw("NO Data \n");
-            }
-            catch(...)
-            {
-                cout<<"NO Data To Erase \n";
-                exit(505);
-            }
-        }
-
-
-    }
-    int size()
-    {
-        return qk.size();
-    }
-    bool empty(){
-        return !size();
-    }
-};
-string simplify(string input){
-    Stack str;
-    string result;
-for(int i=0;i<input.size();i++){
-    if(input[i]=='/'){continue;}
-        string tmp;
-    while(i<input.size()&&input[i]!='/'){
-        tmp +=input[i];
-        ++i;
-        }
-    if(tmp=="."){continue;}
-    else if(tmp==".."){
-        if(!str.empty()){
-            str.pop();
-            }
-            }
-            else{
-                str.push(tmp);
-                }
-        }
-
-        while(!str.empty())
-        {
-            result = "/" + str.top() + result;
-            str.pop();
-        }
-
-        if(result.size() == 0){
-            return "/";
-            }
-
-        return result;
+    for (int i = 0 ; i < a.size(); i++)
+        if (a[i] == rootVal)
+            return i;
+    return -1;
 }
 
-int main(){
-    //1
- cout<<simplify("/home/")<<endl;
-    //2
- cout<<simplify("/../")<<endl;
-    //3
-cout<<simplify("/home//foo/")<<endl;
-    //4
-cout<<simplify("/home/../abc/./foo/")<<endl;
-    //5
-cout<<simplify("/ezz/./ahmed/..////mostafa")<<endl;
+
+void  printPostOrder(string preorder,string inorder)
+{
+    if(inorder.size()==0)
+    {
+        return;
+    }
+    string BEFore_root,AFTER_ROOT;
+    for(int i=inorder.find(preorder[0])+1; i<inorder.size(); i++)
+    {
+        AFTER_ROOT.push_back(inorder[i]);
+    }
+    for(int i=0; i<inorder.find(preorder[0]); i++)
+    {
+        BEFore_root.push_back(inorder[i]);
+    }
+    string preorder_before = preorder , preorder_after = preorder ;
+    for(int i = 0 ; i< AFTER_ROOT.size(); i++)
+    {
+        if(search( preorder_before,AFTER_ROOT[i])!=-1)
+        {
+            preorder_before.erase(search(preorder_before , AFTER_ROOT[i]),1);
+        }
+    }
+    preorder_before.erase(0,1);
+    for(int i=0; i<BEFore_root.size(); i++)
+    {
+        if(search(preorder_after,BEFore_root[i])!=-1)
+        {
+            preorder_after.erase(search(preorder_after,BEFore_root[i]),1);
+        }
+    }
+    preorder_after.erase(0,1);
+
+    printPostOrder(preorder_before,BEFore_root);
+
+    printPostOrder(preorder_after,AFTER_ROOT);
+
+    cout<<preorder[0];
 }
+
+int main()
+{
+    ///testcase_1
+///--------------------
+
+    printPostOrder("ABFGC", "FBGAC");
+    cout<<endl;
+
+    //post : FGBCA
+
+    ///testcase_2
+///--------------------
+
+    printPostOrder("ABCEFDG", "ECFBDAG");
+    cout<<endl;
+
+    //post : EFCDBGA
+
+    ///testcase_3
+///--------------------
+
+    printPostOrder("ABCEFDGHK", "ECFBGDHAK");
+    cout<<endl;
+
+    //post : EFCGHDBKA
+
+    ///testcase_4
+///--------------------
+
+    printPostOrder("ABCED", "BAECD");
+    cout<<endl;
+
+    //post : BEDCA
+
+    ///testcase_5
+///--------------------
+
+    printPostOrder("ABCDEF", "ACBEDF");
+    cout<<endl;
+
+    //post : CEFDBA
+
+
+
+    return 0;
+
+}
+
+
